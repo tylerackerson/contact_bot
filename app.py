92386812d2bc64d927ad58e9163f0046b63db4eb
Tyler Ackerson <tackerson@twilio.com>
2016-06-10 16:59:15 -0700

    Adds twilio IPM

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import os
 from flask import Flask, render_template, jsonify, request
+from faker import Factory
+from twilio.access_token import AccessToken, IpMessagingGrant
+
 app = Flask(__name__)
+fake = Factory.create()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/respond', methods=['POST'])
 def respond():
@@ -14,7 +19,32 @@ def respond():
 
     return jsonify(response=response, user=user, company=company)
 
+@app.route('/token')
+def token():
+    # get credentials for environment variables
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    api_key = os.environ['TWILIO_API_KEY']
+    api_secret = os.environ['TWILIO_API_SECRET']
+    service_sid = os.environ['TWILIO_IPM_SERVICE_SID']
+
+    # create a randomly generated username for the client
+    identity = fake.user_name()
+
+    # Create a unique endpoint ID for the
+    device_id = request.args.get('device')
+    endpoint = "TwilioChatDemo:{0}:{1}".format(identity, device_id)
+
+    # Create access token with credentials
+    token = AccessToken(account_sid, api_key, api_secret, identity)
+
+    # Create an IP Messaging grant and add to token
+    ipm_grant = IpMessagingGrant(endpoint_id=endpoint, service_sid=service_sid)
+    token.add_grant(ipm_grant)
+
+    # Return token info as JSON
+    return jsonify(identity=identity, token=token.to_jwt())
+
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(port=8000)
+    app.run(port=3000)
